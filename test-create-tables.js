d17654b28ee9dddb49b49fbbd36a30364545106a
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-17 20:38:51 -0300

    Remove function that started database

--- test-create-tables.js
+++ test-create-tables.js
@@ -26,15 +26,3 @@ const createTables = async() => {
 }
 
 createTables();
-
-const db = new sqlite.Database('bank.sqlite3', err => {
-    console.log(err, 'init')
-    db.run(`
-        CREATE TABLE categories(
-            id INTEGER PRIMARY KEY NOT NULL,
-            category TEXT
-        );
-    `, err => {
-        return err ? console.log(err) : console.log('Table created!'); 
-    });
-});
