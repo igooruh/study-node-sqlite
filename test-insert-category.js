37c65aa9ba33f2dc0f089b6e20055ea48cfa3c2a
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-18 00:10:31 -0300

    Insert datas on table categories

---
+++ test-insert-category.js
@@ -0,0 +1,13 @@
+const database = require('./services/initial-database');
+
+const insertCategories = async() => {
+
+    const db = await database.initDB('bank.sqlite3');
+
+    await database.run(db, `insert into categories(id, category) values(7, 'smartphone')`);
+    console.log('inserted Category with success!');
+}
+
+insertCategories().catch(err => {
+    console.log(err);
+});
