7131f7fe5b743e1e6dcee1872a35eb4ea6657fb7
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-18 00:49:29 -0300

    Add more one parameter in function run

--- services/initial-database.js
+++ services/initial-database.js
@@ -7,9 +7,9 @@ const initDB = databaseFile => new Promise((resolve, reject) => {
     });
 });
 
-const run = (db, query) => new Promise((resolve, reject) => {
+const run = (db, query, values) => new Promise((resolve, reject) => {
 
-    db.run(query, err => {
+    db.run(query, values, err => {
         err ? reject(err) : resolve(err);
     });
 });
