b6e3f83e5b08e0c25446d29e17735cbfdba98027
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-18 00:50:37 -0300

    Pass datas by array

--- test-insert-category.js
+++ test-insert-category.js
@@ -4,7 +4,7 @@ const insertCategories = async() => {
 
     const db = await database.initDB('bank.sqlite3');
 
-    await database.run(db, `insert into categories(id, category) values(7, 'smartphone')`);
+    await database.run(db, `insert into categories(id, category) values(?, ?)`, [8, 'televisão']);
     console.log('inserted Category with success!');
 }
 
