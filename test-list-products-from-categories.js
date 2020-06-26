07dd3940832b595b5a09f0947909d6604c56c1be
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-06-26 02:17:27 -0300

    List products from categories passing id

---
+++ test-list-products-from-categories.js
@@ -0,0 +1,14 @@
+const database = require('./services/initial-database');
+
+const listProducts = async() => {
+
+    const db = await database.initDB('bank.sqlite3');
+
+    const catId = 7;
+    const products = await database.listAll(db, `select * from products where id in (select product_id from categories_products where category_id = ?)`, [catId]);
+    console.log(`List Products`, products);
+}
+
+listProducts().catch(err => {
+    console.log(err);
+});
