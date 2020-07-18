f1b5f84a00f1d3cf7fd8ee56a3cbf89e5b81891f
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-07-18 01:12:39 -0300

    Creates function findAll

--- model/categories.js
+++ model/categories.js
@@ -6,6 +6,13 @@ const create = async data => {
     await database.queryWithParams(db, `isert into (id, category) values (?, ?)`, data);
 }
 
+const findAll = async() => {
+
+    const db = await database.initDB('../banks.sqlite3');
+    return await database.queryWithParams(db, `select * from category`);
+}
+
 module.exports = {
     create,
+    findAll
 }
