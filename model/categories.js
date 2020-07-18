1d57767b793132dcfe48122841fd35b56b75faf2
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-07-18 01:26:28 -0300

    Add table name

--- model/categories.js
+++ model/categories.js
@@ -2,17 +2,17 @@ const database = require('../services/initial-database');
 
 const create = async data => {
 
-    const db = await database.initDB('../banks.sqlite3');
-    await database.queryWithParams(db, `isert into (id, category) values (?, ?)`, data);
+    const db = await database.initDB('../bankApi.sqlite3');
+    await database.queryWithParams(db, `insert into categories(id, category) values (?, ?)`, data);
 }
 
 const findAll = async() => {
 
-    const db = await database.initDB('../banks.sqlite3');
+    const db = await database.initDB('../bankApi.sqlite3');
     return await database.queryWithParams(db, `select * from category`);
 }
 
 module.exports = {
     create,
-    findAll
+    // findAll
 }
