e72bef04e3d49cd1e9aa857d1d3e5c1951695bdd
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-07-18 00:06:35 -0300

    Creates function runner to persistence on database

--- services/initial-database.js
+++ services/initial-database.js
@@ -5,9 +5,10 @@ const initDB = databaseFile => new Promise((resolve, reject) => {
     const db = new sqlite.Database(databaseFile, err => err ? reject(err) : resolve(db));
 });
 
+const runner = (db, query, values) => new Promise((reject, resolve) =>
+    db.run(query, values), err => err ? reject(err) : resolve());
+
 module.exports = {
     initDB,
-    run,
-    listAllSpecific,
-    listAll
+    runner,
 }
