b753398faf28f3627df6a6e9e92dec2c9e0bd836
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-07-18 01:48:33 -0300

    Test update

--- api.js
+++ api.js
@@ -3,8 +3,9 @@ const categories = require('./model/categories');
 const createCategory = async() => {
 
     await categories.create([1, 'opa']);
-    const cats = await categories.findAll;
+    const cats = await categories.findAll();
     console.log(cats);
     await categories.remove(1);
+    await categories.update(8, ['categoria atualizada']);
 }
-createCategory
+createCategory();
