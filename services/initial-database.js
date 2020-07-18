5dd3724d96e2d33d19ee002276fde3ab6ba3c160
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-07-17 23:50:04 -0300

    Refactoring connection on database

--- services/initial-database.js
+++ services/initial-database.js
@@ -2,26 +2,7 @@ const sqlite = require('sqlite3').verbose();
 
 const initDB = databaseFile => new Promise((resolve, reject) => {
 
-    const db = new sqlite.Database(databaseFile, err => {
-        err ? reject(err) : resolve(db);
-    });
-});
-
-const run = (db, query, values) => new Promise((resolve, reject) => {
-
-    db.run(query, values, err => {
-        err ? reject(err) : resolve(err);
-    });
-});
-
-const listAllSpecific = (db, query, values) => new Promise((resolve, reject) => {
-
-    db.all(query, values, (err, rows) => err ? reject(err) : resolve(rows));
-});
-
-const listAll = (db, query) => new Promise((resolve, reject) => {
-
-    db.all(query, (err, rows) => err ? reject(err) : resolve(rows));
+    const db = new sqlite.Database(databaseFile, err => err ? reject(err) : resolve(db));
 });
 
 module.exports = {
