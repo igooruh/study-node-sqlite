c68d6dbf8fc3938eb944b849c496b8be7b1e8f56
Ígor Pedro Leitão Mascarenhas Martins <igooruh@icloud.com>
2020-07-20 02:30:48 -0300

    Refactory function to input function general

--- model/categories.js
+++ model/categories.js
@@ -1,39 +1,44 @@
 const database = require('../services/initial-database');
 
-const create = async data => {
+const connectDatabase = nameDatabase => {
 
-    const db = await database.initDB('../bankApi.sqlite3');
-    await database.queryWithParams(db, `insert into categories(id, category) values (?, ?)`, data);
+    const create = async data => {
+        
+        const db = await database.initDB(nameDatabase);
+        await database.queryWithParams(db, `insert into categories(id, category) values (?, ?)`, data);
+    }
+    
+    const findAll = async() => {
+        
+        const db = await database.initDB(nameDatabase);
+        return await database.queryAll(db, `select * from category`);
+    }
+    
+    const remove = async id => {
+        
+        const db = await database.initDB(nameDatabase);
+        await database.queryWithParams(db, 'delete from categories where id=?', [id]);
+    }
+    
+    const update = async (id, data) => {
+        
+        const db = await database.initDB(nameDatabase);
+        await database.queryWithParams(db, 'update categories set category=? where id=?', [...data, id]);
+    }
+    
+    const findAllPaginated = async({ pageSize = 1, currentPage = 0 }) => {
+        
+        const db = await database.initDB(nameDatabase);
+        return await database.queryAll(db, `select * from categories limit ${currentPage * pageSize}, ${pageSize}`);
+    }
+
+    return {
+        create,
+        findAll,
+        remove,
+        update,
+        findAllPaginated
+    }
 }
 
-const findAll = async() => {
-
-    const db = await database.initDB('../bankApi.sqlite3');
-    return await database.queryAll(db, `select * from category`);
-}
-
-const remove = async id => {
-
-    const db = await database.initDB('../bankApi.sqlite3');
-    await database.queryWithParams(db, 'delete from categories where id=?', [id]);
-}
-
-const update = async (id, data) => {
-
-    const db = await database.initDB('../bankApi.sqlite3');
-    await database.queryWithParams(db, 'update categories set category=? where id=?', [...data, id]);
-}
-
-const findAllPaginated = async({ pageSize = 1, currentPage = 0 }) => {
-
-    const db = await database.initDB('../bankApi.sqlite3');
-    return await database.queryAll(db, `select * from categories limit ${currentPage * pageSize}, ${pageSize}`);
-}
-
-module.exports = {
-    create,
-    findAll,
-    remove,
-    update,
-    findAllPaginated
-}
+export default connectDatabase;
